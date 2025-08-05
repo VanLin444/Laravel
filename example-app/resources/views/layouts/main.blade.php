bafba6f8ab889924beacb20365502daca04a6b33
VanLin <onel1n.van@yandex.ru>
2025-08-05 22:22:49 +0300

    update page with bootstrap

--- example-app/resources/views/layouts/main.blade.php
+++ example-app/resources/views/layouts/main.blade.php
@@ -9,11 +9,11 @@
 <body>
     <div class="">
         <nav class="">
-            <ul class="">
-                <li><a href="{{ route('main.index')}}">Main</a></li>
-                <li><a href="{{ route('post.index')}}">Posts</a></li>
-                <li><a href="{{ route('contacts.index')}}">Contacts</a></li>
-                <li><a href="{{ route('about.index')}}">About</a></li>
+            <ul class="mb-3 nav nav-tabs">
+                <li class="nav-item"><a href="{{ route('main.index')}}" class="nav-link">Main</a></li>
+                <li class="nav-item"><a href="{{ route('post.index')}}" class="nav-link">Posts</a></li>
+                <li class="nav-item"><a href="{{ route('contacts.index')}}" class="nav-link">Contacts</a></li>
+                <li class="nav-item"><a href="{{ route('about.index')}}" class="nav-link">About</a></li>
             </ul>
         </nav>
     </div>
