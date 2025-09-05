aa5aa34f3e1c8dbd2a92e8a91510eed6660086f4
VanLin <onel1n.van@yandex.ru>
2025-09-05 23:54:43 +0300

    added admin panel

--- example-app/app/Http/Controllers/MainController.php
+++ example-app/app/Http/Controllers/MainController.php
@@ -9,11 +9,6 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     public function index() {
-
-        /* $posts = POST::all();
-        return view('main', compact('posts')); */
-        $post = Post::find(1);
-        $tag = Tag::find(1);
         return view('main');
     }
 }
